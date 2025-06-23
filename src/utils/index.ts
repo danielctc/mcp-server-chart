9163e806b9c15a5b6a50131d2abf2787a63ab46b
danielctc <your-github-email@example.com>
2025-06-23 16:43:03 +0100

    fix: export createBaseHttpHandler from utils index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,4 +3,8 @@ export { generateChartUrl } from "./generate";
 export { zodToJsonSchema } from "./schema";
 export { InMemoryEventStore } from "./InMemoryEventStore";
 export { getBody } from "./getBody";
-export { createBaseHttpServer, type RequestHandlers } from "./httpServer";
+export {
+  createBaseHttpHandler,
+  createBaseHttpServer,
+  type RequestHandlers,
+} from "./httpServer";
